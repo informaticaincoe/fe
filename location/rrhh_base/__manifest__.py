629cba9cab35d9c5fdc0e56705351d30f5004b45
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-06-24 16:00:25 -0600

    Plantilla recibo de salario

--- location/rrhh_base/__manifest__.py
+++ location/rrhh_base/__manifest__.py
@@ -1,16 +1,25 @@
 {
     'name': 'Base Recursos Humanos',
     'depends': ['hr_payroll', 'hr_contract'],
+    'assets': {
+        'web.assets_frontend': [
+            'rrhh_base/static/src/css/inter_font.css',
+            'rrhh_base/static/src/css/bootstrap.min.css',
+        ],
+        'web.assets_pdf': [
+            'rrhh_base/static/src/css/inter_font.css',
+            'rrhh_base/static/src/css/bootstrap.min.css',
+        ],
+    },
     'license': 'GPL-3',
     'category': 'Human Resources',
     'data': [
         'security/ir.model.access.csv',
-        # 'views/hr_inherit_id.xml',
         'views/menu_rrhh_base.xml',
         # 'security/add_group_to_admin.xml',
         "report/report.xml",
         "report/report_payslip_incoe.xml",
     ],
     'installable': True,
-    #"auto_install": True,  #  Consider removing this
+    # "auto_install": True,  #  Consider removing this
 }
