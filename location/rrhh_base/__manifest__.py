028b3cb7697c60425c1c3a2c2af14ed687fe0d8d
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-06-25 00:54:24 -0600

    vistas form y list para retenciones

--- location/rrhh_base/__manifest__.py
+++ location/rrhh_base/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Base Recursos Humanos',
-    'depends': ['hr_payroll', 'hr_contract'],
+    'depends': ['hr_payroll', 'hr_contract', 'l10n_sv_hr_retenciones'],
     'assets': {
         'web.assets_frontend': [
             'rrhh_base/static/src/css/inter_font.css',
@@ -15,8 +15,10 @@
     'category': 'Human Resources',
     'data': [
         'security/ir.model.access.csv',
+        'views/hr_retencion_isss_views.xml',
+        'views/hr_retencion_afp_views.xml',
+        'views/hr_retencion_renta_views.xml',
         'views/menu_rrhh_base.xml',
-        # 'security/add_group_to_admin.xml',
         "report/report.xml",
         "report/report_payslip_incoe.xml",
     ],
