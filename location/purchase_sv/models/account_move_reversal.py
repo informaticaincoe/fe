b84903c3793e3c248beec12a83defb471e5b186d
brenda-incoeit <brenda.chacon@grupoincoe.com>
2025-10-21 17:00:50 -0600

    Unificación de métodos create y write en account.move; generación de ventas y compras en entorno de pruebas y para facturación

--- location/purchase_sv/models/account_move_reversal.py
+++ location/purchase_sv/models/account_move_reversal.py
@@ -51,15 +51,23 @@ class AccountMoveReversal(models.TransientModel):
         else:
             _logger.warning("SIT: Tipo de factura desconocido, no se asigna tipo de diario")
 
+        # Si es factura de compra → usar comportamiento estándar
+        if (move.move_type in (constants.IN_INVOICE, constants.IN_REFUND) and move.journal_id and
+                (not move.journal_id.sit_tipo_documento or move.journal_id.sit_tipo_documento.codigo != constants.COD_DTE_FSE)):
+            _logger.info("SIT: Es factura de compra, se usa flujo estándar de Odoo.")
+            return res
+
         # Si la empresa NO aplica facturación electrónica → usar comportamiento estándar
         if not move.company_id or not move.company_id.sit_facturacion:
             _logger.info("SIT: Empresa no aplica facturación electrónica, se usa flujo estándar de Odoo.")
             return res
 
-        # Si es factura de compra → usar comportamiento estándar
-        if move.move_type in (constants.IN_INVOICE, constants.IN_REFUND):
-            _logger.info("SIT: Es factura de compra, se usa flujo estándar de Odoo.")
-            return res
+        # Asignar tipo de diario según factura
+        if move.move_type in [constants.OUT_INVOICE, constants.OUT_REFUND]:
+            res['journal_type'] = 'sale'
+            _logger.info("SIT: Tipo de diario por defecto = venta")
+        else:
+            _logger.warning("SIT: Tipo de factura desconocido, no se asigna tipo de diario")
 
         # --- Solo aplicar lógica personalizada para facturas de venta ---
         _logger.info("SIT-Purchase: Wizard abierto para reversión de factura ID=%s, tipo=%s, tipo documento=%s", move.id, move.move_type, move.sit_tipo_documento_id)
@@ -73,13 +81,13 @@ class AccountMoveReversal(models.TransientModel):
 
         # Leer tipo de documento de la factura original (sin asignarlo al wizard)
         doc = False
-        if move.move_type == 'in_refund':  # Nota de crédito
+        if move.move_type == constants.OUT_REFUND:  # Nota de crédito
             doc = self.env['account.journal.tipo_documento.field'].search([('codigo', '=', '05')], limit=1)
             _logger.info(
                 "SIT: Tipo de documento original de la factura = %s (Nota de crédito)",
                 doc.valores if doc else "None"
             )
-        elif move.move_type == 'in_debit':  # Nota de débito
+        elif move.move_type == constants.OUT_INVOICE:  # Nota de débito
             doc = self.env['account.journal.tipo_documento.field'].search([('codigo', '=', '06')], limit=1)
             _logger.info(
                 "SIT: Tipo de documento original de la factura = %s (Nota de débito)",
