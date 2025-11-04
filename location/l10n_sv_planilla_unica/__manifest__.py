7d5acd8491f7431d1d02ea7348b5076f00afa80d
Karen-Burgos <karen.burgos@grupoincoe.com>
2025-11-04 17:00:25 -0600

    modulo planilla unica

---
+++ location/l10n_sv_planilla_unica/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "RRHH - Planilla Única",
+    "depends": [
+        "hr",
+        "hr_payroll",
+        "rrhh_base",
+        "l10n_sv_hr_asignaciones",
+        "l10n_sv_hr_retenciones",
+    ],
+    'assets': {
+        'web.assets_pdf': [
+            'rrhh_base/static/src/css/inter_font.css',
+            'rrhh_base/static/src/css/bootstrap.min.css',
+        ],
+    },
+    'license': 'GPL-3',
+    'category': 'Human Resources',
+    'data': [
+        'security/ir.model.access.csv',
+        "views/hr_payslip_menu.xml",
+        "views/hr_employees_planilla_unica_data.xml",
+        "views/hr_payslip_planilla_unica_reporte.xml",
+    ],
+    'installable': True,
+}
