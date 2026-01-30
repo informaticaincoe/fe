44adb986662d56d33c51af220dd4220bcbe24243
Karen-Burgos <karen.burgos@grupoincoe.com>
2026-01-30 09:31:45 -0600

    Mapa de zonas en depacho

--- location/l10n_sv_despacho/models/dispatch_route.py
+++ location/l10n_sv_despacho/models/dispatch_route.py
@@ -151,6 +151,21 @@ class DispatchRoute(models.Model):
 
     #########
 
+    def action_download_report_reception(self):
+        self.ensure_one()
+
+        print(">>>>>>> SELF ", self )
+        print(">>>>>>> SELF id", self.id )
+
+        ruta = self.env["dispatch.route"].search([
+            ("id", "=", self.id),
+        ], limit=1)
+
+        print(">>>>>>> RUTA ", ruta )
+        print(">>>>>>> RUTA ID ", ruta.id )
+
+        return self.env.ref('l10n_sv_despacho.action_report_recepcion_ruta').report_action(ruta)
+
 
 
 
--- location/l10n_sv_despacho/static/src/js/map_field.js
+++ location/l10n_sv_despacho/static/src/js/map_field.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
-import { Component, onMounted, useRef } from "@odoo/owl";
+import { Component, onMounted, useRef, onWillUpdateProps } from "@odoo/owl";
 
 export class MapDistrictsComponent extends Component {
   static template = "l10n_sv_despacho.MapContainer";
@@ -9,55 +9,64 @@ export class MapDistrictsComponent extends Component {
 
   setup() {
     this.mapRef = useRef("map");
+    this.rootRef = useRef("root");
+    this.map = null;
+
+    onWillUpdateProps((nextProps) => {
+        if (this.map) {
+            this._applyStyles(this.map, nextProps);
+        }
+    });
 
     onMounted(async () => {
-        // Intentar esperar un poco si google no está listo inmediatamente
-        let attempts = 0;
-        const checkGoogle = setInterval(async () => {
-            attempts++;
-            if (typeof google !== "undefined") {
-                clearInterval(checkGoogle);
-                await this.initMap();
-            } else if (attempts > 50) { // Si después de 5 seg no carga, dar error
-                clearInterval(checkGoogle);
-                console.error("Google Maps API no cargó después de varios intentos.");
-            }
-        }, 100);
+        if (this.el) {
+            this.rootRef.el.style.width = "100vw";
+            this.rootRef.el.style.display = "block";
+        }
+        console.log("mapa ", this)
+
+        await this._loadGoogleAPI();
+        this._initMap();
     });
   }
 
-  async initMap() {
+  async _loadGoogleAPI() {
+    if (window.google) return;
+    return new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCrGkTd0pXFZ1lZbj4DJrmsnmmXvT_DKjg`;
+        script.async = true;
+        script.onload = resolve;
+        document.head.appendChild(script);
+    });
+  }
 
+  _initMap() {
+    const mapOptions = { center: { lat: 13.794, lng: -88.896 }, zoom: 8 };
+    this.map = new google.maps.Map(this.mapRef.el, mapOptions);
+
+    // Cargar del archivo geojson
+    this.map.data.loadGeoJson("/l10n_sv_despacho/static/data/slv_admin2_em.geojson");
+    this._applyStyles(this.map, this.props);
+  }
+
+  _applyStyles(map, props) {
     let districts = [];
     try {
-      districts = JSON.parse(this.props.record.data.selected_districts_json || "[]");
-    } catch (e) {}
-
-    const mapOptions = { center: { lat: 13.794, lng: -88.896 }, zoom: 8 };
-    const map = new google.maps.Map(this.mapRef.el, mapOptions);
-
-    map.data.loadGeoJson(
-      "/l10n_sv_despacho/static/data/slv_admin2.geojson",
-      null,
-      (features) => {
-
-        // Opcional: hacer fit bounds
-        const bounds = new google.maps.LatLngBounds();
-        features.forEach(f => {
-          f.getGeometry().forEachLatLng((latlng) => bounds.extend(latlng));
-        });
-        if (!bounds.isEmpty()) map.fitBounds(bounds);
-      }
-    );
+      // leer codigos de los distritos selecionados
+      districts = JSON.parse(props.record.data.selected_districts_json || "[]");
+    } catch (e) { districts = []; }
 
     map.data.setStyle((feature) => {
-      const name = feature.getProperty("adm3_name");
-      const isSelected = districts.includes(name);
+      //obtener codigo de distrito del archivo geojson
+      const pcode = feature.getProperty("adm2_pcode");
+      //obtener distritos que coincidan su codigo con el codigo del geojson
+      const isSelected = districts.includes(pcode);
       return {
-        fillColor: isSelected ? "#714B67" : "transparent",
-        strokeColor: isSelected ? "#714B67" : "#9E9E9E",
+        fillColor: isSelected ? "#9741CC" : "transparent",
+        strokeColor: isSelected ? "#9741CC" : "#9E9E9E",
         strokeWeight: isSelected ? 2 : 0.5,
-        fillOpacity: isSelected ? 0.5 : 0,
+        fillOpacity: isSelected ? 0.6 : 0,
         visible: true,
       };
     });
